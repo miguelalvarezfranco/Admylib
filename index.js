d950b1f6b6548811feeac69bfeadd3827035618e
MiguelAngel <miguel12061@outlook.com>
2023-12-23 00:18:44 -0500

    mercado pago

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const {body,  validationResult} = require('express-validator');
@@ -125,56 +126,6 @@ app.post("/create_preference", (req, res) => {
 // 		MerchantOrder: req.query.merchant_order_id
 // 	});
 // });
-
-
-
-
-
-
-// app.post('/registrarlibro',[
-//     body('isbn', 'ingrese solo numeros')
-//     .exists()
-//     .isNumeric({min:10}),
-//     body('titulo','ingrese solo letras')
-//     .exists()
-//     .isLength({min:100}),
-//     body('Añodepublicacion','ingrese un año correcto')
-//     .exists()
-//     .isLength({min:50}),
-//     body('editorial','ingrese solo letras')
-//     .exists()
-//     .isLength({min:50}),
-//     body('copiasdisponibles','ingrese numeros ')
-//     .exists()
-//     .isNumeric({min:50}),
-//     body('precio','en este campo no se permiten letras')
-//     .exists()
-//     .isNumeric({min:100}),
-//     body('idioma','ingrese solo letras')
-//     .exists()
-//     .isLength({min:100}),
-//     body('autor','ingrese un solo letras')
-//     .exists()
-//     .isLength({min:100}),
-//     body('iamgen','ingrese solo  imagenes ')
-//     .exists()
-//     .isLength({min:80}),
-//     body('materias','ingrese solo letras ')
-//     .exists()
-//     .isLength({min:100}),
-//     ],(req, res)=>{
-
-//     const errors = validationResult(req)
-//     if(!errors.isEmpty()) {
-//         console.log(req.body)
-//         const valores = req.body
-//         const validaciones = errors.array
-//         res.render('libros', {validaciones:validaciones, valores:valores})
-//     }else{
-//         res.send('¡Validacion exitosa!')
-//     }
-// })
-
 
 module.exports = app;
 
